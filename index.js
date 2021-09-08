21655bbbb47e54c8037ba5546d02cb0df346b0a1
+ <+>
2021-09-08 08:16:35 -0500

    completed assignmnent

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(function (response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(function(books) {
+      renderBooks(books)
+  });
 }
 
 function renderBooks(books) {
